abe915bf9a42ff955aed0fda2435e0e29912b7b1
Molly Nacey <mnacey@princeton.edu>
2013-01-04 03:10:03 -0500

    change settings

--- HistoryOnline/settings.py
+++ HistoryOnline/settings.py
@@ -14,8 +14,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.abspath(os.path.join(PWD, '..', 'store.db')), # Or path to database file if using sqlite3.
+        'ENGINE': '',#django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '',#os.path.abspath(os.path.join(PWD, '..', 'store.db')), # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
